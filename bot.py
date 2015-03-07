646d6fbe7b77c667161fb26d16fb53bd74b79b57
Ross Goodwin <ross.goodwin@gmail.com>
2015-03-07 10:22:46 -0500

    added files

---
+++ bot.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import tweepy
+import json
+from time import sleep
+import string
+import re
+
+N = 206
+
+CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+
+ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+ijFile = open('ij.json', 'r')
+ijTokens = json.load(ijFile)
+ijFile.close()
+
+# print "SLEEPING 15 MIN..."
+# sleep(900)
+# print "...DONE SLEEPING"
+
+i = N
+for token in ijTokens[N:]:
+
+	candidates = [] # list of result objects
+	while not candidates:
+		try:
+			results = api.search(q=token)
+
+			for r in results:
+				# print r.text + "\n"
+				if re.match("\.?[@#]?%s([^a-z]|$)"%token,r.text.lower()) is not None:
+					# print "\n\nMATCH FOUND\n\n"
+					candidates.append(r)
+
+			sleep(5)
+
+		except tweepy.TweepError:
+			# print "\n\nRATE LIMIT REACHED\n\n"
+			sleep(60)
+			continue
+
+	api.retweet(candidates[0].id)
+
+	# print "TWEETED WORD " + str(i)
+
+	sleep(60)
+
+	i += 1
+
+
+
+
+
